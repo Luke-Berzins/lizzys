0f157cb403127626c81d4f1ce396bd4c319e693a
Luke-Berzins <lukeberzins16@gmail.com>
2021-05-31 12:37:50 -0400

    basic setup

---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react' 
+import { Link } from 'react-router-dom'
+export default function Nav(props) {
+
+
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/cooking">cooking</Link>
+                </li>
+                <li>
+                    <Link to="/running">running</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
