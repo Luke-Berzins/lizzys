0e5eda248672860b0a9870d7a07b8bc50ac193ed
Luke-Berzins <lukeberzins16@gmail.com>
2021-06-07 12:46:30 -0400

    all default recipes ingredients populated

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ const cookingPosts = {
     1 : {
         id: 1,
         title : "Millionaire Shortbread",
-        date: "June 02 2021",
-        mainImage : "https://picsum.photos/seed/picsum/200/300",
+        date: "December 14 2021",
+        categories: ["desert"],
+        mainImage : "./recipe_photos/Millionaires-shortbread.jpeg",
         options: {
 
         },
@@ -25,19 +26,39 @@ const cookingPosts = {
           { 
             type : "list",
             image : "https://picsum.photos/seed/picsum/200/300",
-            title : "Ingredients",
-            items : ["potatoes", "carrots", "shallots", "fun-stuff"]
+            title : "Shortbread Crust",
+            items : ["1 cup unsalted butter, softened (226g)", "⅓ cup sugar (70g)", "⅓ cup light brown sugar, packed (70g)", "1 large egg yolk", "¾ teaspoon vanilla extract", "½ teaspoon salt", "2 ¼ cups all-purpose flour (260g)"]
           },
           { 
-            type : "left-image",
+            type : "list",
             image : "https://picsum.photos/seed/picsum/200/300",
-            text : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            title : "Caramel",
+            items : ["2 14-oz cans sweetened condensed milk (792g)", "14 Tablespoons butter cut into Tablespoon-sized pieces (198g)", "1 cup light brown sugar packed (200g)", "⅓ cup light corn syrup (80ml)", "1 teaspoon vanilla extract", "¼ teaspoon salt"]
           },
           { 
-            type : "text",
+            type : "list",
             image : "https://picsum.photos/seed/picsum/200/300",
-            text : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            title : "Chocolate Ganache",
+            items : ["2 cups semisweet chocolate chips (340g)", "½ cup heavy cream (120ml)", "½ teaspoon vanilla extract (5ml)", "sea salt for sprinkling"]
           },
+          { 
+            type : "list",
+            image : "https://picsum.photos/seed/picsum/200/300",
+            title : "Shortbread Crust Instructions",
+            items : ["Preheat oven to 350F (177C) and line a 13x9 baking pan with parchment paper.", "Using an electric mixer, beat butter until well creamed.", "Add sugars and beat until light and fluffy, about 30 seconds.", "Add egg yolk and vanilla extract and stir well, pausing to scrape down the sides and mixing until ingredients are well-combined.", "Add flour, gradually (about ½ cup at a time), pausing to scrape down sides of bowl. Halfway through the addition of the flour, sprinkle in the salt with the beater still running.", "Don't over-beat the dough, you want it to be slightly crumbly to make it easier to press into the pan.", "Drop dough over prepared pan and gently, firmly, evenly, press dough into the bottom of the pan.  To make it easier, I usually lay a piece of wax paper over the dough and use the palm of my hand to smooth the surface (and then discard wax paper).", "Bake on 350F (177C) for 20-25 minutes -- edges should be lightly golden brown.", "Allow to cool while you prepare your caramel topping."]
+          },
+          { 
+            type : "list",
+            image : "https://picsum.photos/seed/picsum/200/300",
+            title : "Caramel Instructions",
+            items : ["Combine condensed milk, butter, brown sugar, and corn syrup in a medium-sized saucepan over medium heat.", "Stir frequently until butter is melted and ingredients are well-combined. Continue to stir constantly until mixture begins to boil (this could take several minutes, but you don't need to increase the heat).", "Once mixture comes to a boil, reduce heat to a simmer (slowly bubbling), still stirring constantly.  Continue to stir 10-15 minutes until mixture turns a rich caramel color and has thickened (should begin pulling away from the sides of the pot as you stir).", "Remove from heat and immediately stir in the vanilla extract and salt.", "Pour evenly over prepared shortbread, use a knife to spread evenly if needed.", "Allow to cool at room temperature for several hours or in the refrigerator for 1 hour before topping with chocolate."]
+          },
+          { 
+            type : "list",
+            image : "https://picsum.photos/seed/picsum/200/300",
+            title : "Chocolate Ganache Instructions",
+            items : ["Combine chocolate chips and heavy cream in a small saucepan over medium heat.  ", "Stir frequently until chocolate is melted and mixture is smooth.  ", "Remove from heat and stir in vanilla extract.  Allow chocolate to cool just slightly for about 5 minutes, and then evenly spread over prepared caramel layer.  Wait several minutes and then sprinkle with sea salt.  ", "Allow chocolate to harden (I recommend placing in refrigerator for about 30 minutes -- my family prefers these treats refrigerated anyway) before cutting and serving."]
+          }, 
         ] 
       
       }, 
@@ -45,7 +66,8 @@ const cookingPosts = {
         id : 2,
         title : "Peanut Sauce Stir-Fry",
         date: "June 02 2021",
-        mainImage : "https://picsum.photos/seed/picsum/200/300",
+        categories: ["dinner"],
+        mainImage : "./recipe_photos/peanut-sauce-stir-fry.jpeg",
         options: {
                   
         },
@@ -54,7 +76,7 @@ const cookingPosts = {
             type : "list",
             image : "https://picsum.photos/seed/picsum/200/300",
             title : "Ingredients",
-            items : ["1 cup white rice flour", "1/2 teaspoon ground", "1/2 teaspoon paprika ","1/2 teaspoon kosher salt ","1/4 teaspoon freshly ground black pepper ","1 medium head cauliflower, cut into 2- to 3-inch florets, stems reserved for another use ","1/2 cup barbecue sauce","2 tablespoons unsalted butter "]
+            items : ["1/4 cup peanut butter","1/2 cup applesauce","1/4 cup tamari or soy sauce","2 cloves of garlic or 1 teaspoon garlic powder","1/2 teaspoon minced ginger","1/8 teaspoon cayenne pepper (optional)"]
           },
           { 
             type : "",
@@ -66,7 +88,8 @@ const cookingPosts = {
     3: {
         id : 3,
         title : "Power Salad",
-        date: "June 02 2021",
+        date: "Auguest 25 2021",
+        categories: ["dinner"],
         mainImage : "./recipe_photos/power-salad.jpeg",
         options: {
                   
@@ -92,7 +115,8 @@ const cookingPosts = {
     4: {
         id : 4,
         title : "Buffalo Cauliflower",
-        date: "June 02 2021",
+        date: "April 04 2021",
+        categories: ["dinner"],
         mainImage : "./recipe_photos/butter-cauliflower.jpeg",
         options: {
                   
