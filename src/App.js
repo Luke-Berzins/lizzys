3c34de9d011a068bf7384aa21ba400659f120234
Luke-Berzins <lukeberzins16@gmail.com>
2021-06-07 12:23:01 -0400

    adding initial content to recipes page

--- src/App.js
+++ src/App.js
@@ -51,9 +51,10 @@ const cookingPosts = {
         },
         content: [
           { 
-            type : "",
+            type : "list",
             image : "https://picsum.photos/seed/picsum/200/300",
-            text : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            title : "Ingredients",
+            items : ["1 cup white rice flour", "1/2 teaspoon ground", "1/2 teaspoon paprika ","1/2 teaspoon kosher salt ","1/4 teaspoon freshly ground black pepper ","1 medium head cauliflower, cut into 2- to 3-inch florets, stems reserved for another use ","1/2 cup barbecue sauce","2 tablespoons unsalted butter "]
           },
           { 
             type : "",
@@ -66,20 +67,25 @@ const cookingPosts = {
         id : 3,
         title : "Power Salad",
         date: "June 02 2021",
-        mainImage : "https://picsum.photos/seed/picsum/200/300",
+        mainImage : "./recipe_photos/power-salad.jpeg",
         options: {
                   
         },
         content: [
           { 
-            type : "",
-            image : "https://picsum.photos/seed/picsum/200/300",
-            text : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            type : "list",
+            title : "Salad",
+            items : ["1 cup quinoa", "1 bunch kale, ribs removed and chopped into very small, bite-sized pieces", "2 tablespoons olive oil", "1 medium lime, juiced", "1/2 teaspoon salt", "1 (14-ounce) can black beans, rinsed and drained, or 1 ½ cups cooked black beans", "1/3 cup crumbled feta, omit for vegan/dairy-free salad", "1/4 cup pepitas (green pumpkin seeds)"]
           },
           { 
-            type : "",
-            image : "https://picsum.photos/seed/picsum/200/300",
-            text : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            type : "list",
+            title : "Sweet potatoes",
+            items : ["1 cup white rice flour", "1/2 teaspoon ground", "1/2 teaspoon paprika ","1/2 teaspoon kosher salt ","1/4 teaspoon freshly ground black pepper ","1 medium head cauliflower, cut into 2- to 3-inch florets, stems reserved for another use ","1/2 cup barbecue sauce","2 tablespoons unsalted butter "]
+          },
+          { 
+            type : "list",
+            title : "Avocado sauce",
+            items : ["2 avocados, sliced into long strips", "2 limes, juiced", "2 tablespoons olive oil", "1 medium jalapeño, deseeded, membranes removed and roughly chopped", "1 handful cilantro leaves", "½ teaspoon ground coriander, optional", "Salt, to taste",]
           },
         ]
     }, 
@@ -87,15 +93,16 @@ const cookingPosts = {
         id : 4,
         title : "Buffalo Cauliflower",
         date: "June 02 2021",
-        mainImage : "https://picsum.photos/seed/picsum/200/300",
+        mainImage : "./recipe_photos/butter-cauliflower.jpeg",
         options: {
                   
         },
         content: [
           { 
-            type : "",
+            type : "list",
             image : "https://picsum.photos/seed/picsum/200/300",
-            text : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            title : "Ingredients",
+            items : ["1 cup white rice flour", "1/2 teaspoon ground", "1/2 teaspoon paprika ","1/2 teaspoon kosher salt ","1/4 teaspoon freshly ground black pepper ","1 medium head cauliflower, cut into 2- to 3-inch florets, stems reserved for another use ","1/2 cup barbecue sauce","2 tablespoons unsalted butter "]
           },
           { 
             type : "",
