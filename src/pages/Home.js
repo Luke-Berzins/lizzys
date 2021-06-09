521418507977b96da10682e011ce4344dc1e1444
Luke-Berzins <lukeberzins16@gmail.com>
2021-06-09 13:15:01 -0400

    adding lottie package and initial lottie in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import lottie from 'lottie-web'
 import './Home.css'
 
 export default function Home(props) {
 
+    const lottieShoePot = useRef(null)
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: lottieShoePot.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('./shoe-cooking.json')
+        })
+    }, [])
+
     return (
-        <section>
+        <section className="home__container">
             <h1>Good Health & Food</h1>
+            <div className="hero__lottie" ref={lottieShoePot}></div>
             <div className="hero">
                 <p className="hero__text">
                     Turning Food...
